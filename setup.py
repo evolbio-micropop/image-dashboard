1ffed5198a85dec26cecaadac8e8911993bddde1
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2019-04-12 14:24:02 +0200

    Removing setuptools_scm dependency.

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from setuptools import setup
 
 setup(
     name='image_dashboard',
-    use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    packages=['image_dashboard',
-              ],
+    packages=['image_dashboard', ],
     license='MIT',
     long_description=open('README.md').read(),
 )
